04a27ac302ee0ee5fdd946e8403975d838e6d2d7
Jan Kolkmeier <jankolkmeier@gmail.com>
2016-12-15 20:52:28 +0100

    initial commit twitter2middleware

---
+++ java/Twitter2Middleware/src/hmi/Twitter2Middleware/TestGui.java
@@ -0,0 +1,45 @@
+package hmi.Twitter2Middleware;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class TestGui {
+	
+	public static void main(String[] args) {
+		TweetForwarder tf = new TweetForwarder();
+		new TestGui(tf);
+	}
+
+	public TestGui(TweetForwarder tf) {
+		JFrame guiFrame = new JFrame();
+	
+		guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		guiFrame.setTitle("Simulate tweets");
+		guiFrame.setSize(445,60);
+		
+		guiFrame.setLocationRelativeTo(null);
+		
+		JTextField textField = new JTextField(30);
+		JButton sendButton = new JButton( "Send");
+	
+		sendButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				// send
+				tf.Forward("fakeUser", textField.getText(), System.currentTimeMillis() / 1000L);
+				System.out.println("Send: "+textField.getText());
+			}
+		});
+	
+		guiFrame.add(textField, BorderLayout.WEST);
+		guiFrame.add(sendButton,BorderLayout.EAST);
+	
+		//make sure the JFrame is visible
+		guiFrame.setVisible(true);
+	}
+
+}
---
+++ java/Twitter2Middleware/src/hmi/Twitter2Middleware/TweetForwarder.java
@@ -0,0 +1,257 @@
+package hmi.Twitter2Middleware;
+
+import static nl.utwente.hmi.middleware.helpers.JsonNodeBuilders.object;
+
+import java.util.List;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import nl.utwente.hmi.middleware.Middleware;
+import nl.utwente.hmi.middleware.MiddlewareListener;
+import nl.utwente.hmi.middleware.stomp.STOMPMiddleware;
+import twitter4j.*;
+
+public final class TweetForwarder implements MiddlewareListener {
+	Middleware middleware;
+	
+	public TweetForwarder() {
+		this("127.0.0.1", 61613, "/topic/Tweets", "/topic/Twitter2MiddlewareCMD");
+		
+        Twitter twitter = new TwitterFactory().getInstance();
+        try {
+            List<Status> statuses;
+            String user = twitter.verifyCredentials().getScreenName();
+            statuses = twitter.getUserTimeline();
+            System.out.println("Showing @" + user + "'s user timeline.");
+            for (Status status : statuses) {
+                System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
+            }
+        } catch (TwitterException te) {
+            te.printStackTrace();
+            System.out.println("Failed to get timeline: " + te.getMessage());
+            System.exit(-1);
+        }
+	}
+	
+	public TweetForwarder(String ip, int port, String targetTopic, String feedbackTopic) {
+		middleware = new STOMPMiddleware(ip, port, targetTopic, feedbackTopic);
+		middleware.addListener(this);
+	}
+	
+	public void Forward(String user, String content, long time) {
+      	JsonNode msg = object(
+    			"time", time+"",
+    			"user", user,
+    			"content", content).end();
+		middleware.sendData(msg);
+	}
+	
+
+	@Override
+	public void receiveData(JsonNode jn) {
+		System.out.println("/topic/Twitter2MiddlewareCMD: "+jn.toString());
+
+		if (jn.has("cmd") && jn.get("cmd").asText().equals("tweet") && jn.has("content")) {
+			// tweet jn.get("content").asText()
+			// use the safe queue / abstract worker implementation?
+		}
+	}
+	
+	
+	/*
+    public static void main(String[] args) throws TwitterException {
+        TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+        twitterStream.addListener(listener);
+        // user() method internally creates a thread which manipulates TwitterStream and calls these adequate listener methods continuously.
+        twitterStream.user();
+    }
+
+    private static final UserStreamListener listener = new UserStreamListener() {
+        @Override
+        public void onStatus(Status status) {
+            System.out.println("onStatus @" + status.getUser().getScreenName() + " - " + status.getText());
+        }
+
+        @Override
+        public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+            System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
+        }
+
+        @Override
+        public void onDeletionNotice(long directMessageId, long userId) {
+            System.out.println("Got a direct message deletion notice id:" + directMessageId);
+        }
+
+        @Override
+        public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+            System.out.println("Got a track limitation notice:" + numberOfLimitedStatuses);
+        }
+
+        @Override
+        public void onScrubGeo(long userId, long upToStatusId) {
+            System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+        }
+
+        @Override
+        public void onStallWarning(StallWarning warning) {
+            System.out.println("Got stall warning:" + warning);
+        }
+
+        @Override
+        public void onFriendList(long[] friendIds) {
+            System.out.print("onFriendList");
+            for (long friendId : friendIds) {
+                System.out.print(" " + friendId);
+            }
+            System.out.println();
+        }
+
+        @Override
+        public void onFavorite(User source, User target, Status favoritedStatus) {
+            System.out.println("onFavorite source:@"
+                + source.getScreenName() + " target:@"
+                + target.getScreenName() + " @"
+                + favoritedStatus.getUser().getScreenName() + " - "
+                + favoritedStatus.getText());
+        }
+
+        @Override
+        public void onUnfavorite(User source, User target, Status unfavoritedStatus) {
+            System.out.println("onUnFavorite source:@"
+                + source.getScreenName() + " target:@"
+                + target.getScreenName() + " @"
+                + unfavoritedStatus.getUser().getScreenName()
+                + " - " + unfavoritedStatus.getText());
+        }
+
+        @Override
+        public void onFollow(User source, User followedUser) {
+            System.out.println("onFollow source:@"
+                + source.getScreenName() + " target:@"
+                + followedUser.getScreenName());
+        }
+
+        @Override
+        public void onUnfollow(User source, User followedUser) {
+            System.out.println("onFollow source:@"
+                + source.getScreenName() + " target:@"
+                + followedUser.getScreenName());
+        }
+
+        @Override
+        public void onDirectMessage(DirectMessage directMessage) {
+            System.out.println("onDirectMessage text:"
+                + directMessage.getText());
+        }
+
+        @Override
+        public void onUserListMemberAddition(User addedMember, User listOwner, UserList list) {
+            System.out.println("onUserListMemberAddition added member:@"
+                + addedMember.getScreenName()
+                + " listOwner:@" + listOwner.getScreenName()
+                + " list:" + list.getName());
+        }
+
+        @Override
+        public void onUserListMemberDeletion(User deletedMember, User listOwner, UserList list) {
+            System.out.println("onUserListMemberDeleted deleted member:@"
+                + deletedMember.getScreenName()
+                + " listOwner:@" + listOwner.getScreenName()
+                + " list:" + list.getName());
+        }
+
+        @Override
+        public void onUserListSubscription(User subscriber, User listOwner, UserList list) {
+            System.out.println("onUserListSubscribed subscriber:@"
+                + subscriber.getScreenName()
+                + " listOwner:@" + listOwner.getScreenName()
+                + " list:" + list.getName());
+        }
+
+        @Override
+        public void onUserListUnsubscription(User subscriber, User listOwner, UserList list) {
+            System.out.println("onUserListUnsubscribed subscriber:@"
+                + subscriber.getScreenName()
+                + " listOwner:@" + listOwner.getScreenName()
+                + " list:" + list.getName());
+        }
+
+        @Override
+        public void onUserListCreation(User listOwner, UserList list) {
+            System.out.println("onUserListCreated  listOwner:@"
+                + listOwner.getScreenName()
+                + " list:" + list.getName());
+        }
+
+        @Override
+        public void onUserListUpdate(User listOwner, UserList list) {
+            System.out.println("onUserListUpdated  listOwner:@"
+                + listOwner.getScreenName()
+                + " list:" + list.getName());
+        }
+
+        @Override
+        public void onUserListDeletion(User listOwner, UserList list) {
+            System.out.println("onUserListDestroyed  listOwner:@"
+                + listOwner.getScreenName()
+                + " list:" + list.getName());
+        }
+
+        @Override
+        public void onUserProfileUpdate(User updatedUser) {
+            System.out.println("onUserProfileUpdated user:@" + updatedUser.getScreenName());
+        }
+
+        @Override
+        public void onUserDeletion(long deletedUser) {
+            System.out.println("onUserDeletion user:@" + deletedUser);
+        }
+
+        @Override
+        public void onUserSuspension(long suspendedUser) {
+            System.out.println("onUserSuspension user:@" + suspendedUser);
+        }
+
+        @Override
+        public void onBlock(User source, User blockedUser) {
+            System.out.println("onBlock source:@" + source.getScreenName()
+                + " target:@" + blockedUser.getScreenName());
+        }
+
+        @Override
+        public void onUnblock(User source, User unblockedUser) {
+            System.out.println("onUnblock source:@" + source.getScreenName()
+                + " target:@" + unblockedUser.getScreenName());
+        }
+
+        @Override
+        public void onRetweetedRetweet(User source, User target, Status retweetedStatus) {
+            System.out.println("onRetweetedRetweet source:@" + source.getScreenName()
+                + " target:@" + target.getScreenName()
+                + retweetedStatus.getUser().getScreenName()
+                + " - " + retweetedStatus.getText());
+        }
+
+        @Override
+        public void onFavoritedRetweet(User source, User target, Status favoritedRetweet) {
+            System.out.println("onFavroitedRetweet source:@" + source.getScreenName()
+                + " target:@" + target.getScreenName()
+                + favoritedRetweet.getUser().getScreenName()
+                + " - " + favoritedRetweet.getText());
+        }
+
+        @Override
+        public void onQuotedTweet(User source, User target, Status quotingTweet) {
+            System.out.println("onQuotedTweet" + source.getScreenName()
+                + " target:@" + target.getScreenName()
+                + quotingTweet.getUser().getScreenName()
+                + " - " + quotingTweet.getText());
+        }
+
+        @Override
+        public void onException(Exception ex) {
+            ex.printStackTrace();
+            System.out.println("onException:" + ex.getMessage());
+        }
+    };*/
+}
