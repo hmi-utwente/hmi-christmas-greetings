c9ab1be04f64b475a167dc07a492725ab87996c5
Daniel <d.p.davison@utwente.nl>
2016-12-15 20:55:21 +0100

    added outline for TwitterInterpreter project

---
+++ java/TwitterInterpreter/src/twitter/interpreter/TwitterInterpreter.java
@@ -0,0 +1,12 @@
+package twitter.interpreter;
+
+public class TwitterInterpreter {
+
+	private static final String INPUT_TOPIC = "/topic/Tweets";
+	private static final String OUTPUT_TOPIC = "/topic/BehaviourRequests";
+	
+	public TwitterInterpreter(){
+		
+	}
+	
+}
